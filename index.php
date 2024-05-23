8b5d4bdbe8dae4649a19c5f136539b15173bca9a
seeejaay <carljustinem984@gmail.com>
2024-05-23 15:06:26 +0800

    updates

--- index.php
+++ index.php
@@ -30,28 +30,45 @@ if (isset($_POST['add'])) {
 <html>
 <head>
     <title>HJT Electronic Devices Store</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h1>Welcome to HJT Electronic Devices Store</h1>
-    <?php if (isset($message)): ?>
-        <p><?php echo $message; ?></p>
-    <?php endif; ?>
-    <h2>Add Device</h2>
-    <form method="post">
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" required><br>
-        <label for="price">Price:</label>
-        <input type="number" name="price" id="price" required><br>
-        <label for="description">Description:</label>
-        <input type="text" name="description" id="description" required><br>
-        <button type="submit" name="add">Add Device</button>
-    </form>
-    <h2>Navigation</h2>
-    <ul>
-        <li><a href="view_dev.php">View Devices</a></li>
-        <li><a href="pur_dev.php">Purchase Devices</a></li>
-        <li><a href="pur_his.php">View Purchase History</a></li>
-        <li><a href="dev_details.php">View Device Details</a></li>
-    </ul>
+    <div class="container">
+        <div class="header">
+            <h1>HJT Electronic Devices Store</h1>
+        </div>
+        
+        <div class="card">
+            <h2>Add Device</h2>
+            <?php if (isset($message)): ?>
+                <p><?php echo $message; ?></p>
+            <?php endif; ?>
+            <form method="post">
+                <div class="input-group">
+                    <label for="name">Name:</label>
+                    <input type="text" class="input" name="name" id="name" required><br>
+                </div>
+                <div class="input-group">
+                    <label for="price">Price:</label>
+                    <input type="number" class="input" name="price" id="price" required><br>
+                </div>
+                <div class="input-group">
+                    <label for="description">Description:</label>
+                    <input type="text" class="input" name="description" id="description" required><br>
+                </div>
+                <div class="btn-group">
+                    <button type="submit" class="btn-submit" name="add">Add Device</button>
+                </div>
+            </form>
+
+        </div>
+        <h2>Navigation</h2>
+        <ul>
+            <li><a href="view_dev.php">View Devices</a></li>
+            <li><a href="pur_dev.php">Purchase Devices</a></li>
+            <li><a href="pur_his.php">View Purchase History</a></li>
+            <li><a href="dev_details.php">View Device Details</a></li>
+        </ul>
+    </div>
 </body>
 </html>
